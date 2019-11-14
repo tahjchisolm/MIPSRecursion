07ca51b082ae7845be62f6bd5b06f01fe220a261
Tahj <tahjchisolm21@gmail.com>
2019-11-13 19:52:36 -0500

    collected user input

--- project2.s
+++ project2.s
@@ -11,3 +11,9 @@ main:
 	li $v0, 4
 	la, $a0, myMessage # print Message
 	syscall
+
+	li $v0, 8 # take in input
+	la $a0, buffer # load byte space
+	li $a1, 11 # create space for string
+	move $t0, $a0 # save string to t0
+	syscall
