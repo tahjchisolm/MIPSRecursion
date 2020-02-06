d99c0809d28d4f209c83e43092060994a6625481
Chisolm <tahj.chisolm@iLABMAC-111.local>
2020-02-06 17:48:13 -0500

    created a timer class

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,9 +1,39 @@
 // Creating
 #include <iostream>
 #include <fstream>
+#include memory
+#include <chrono>
 
 using namespace std;
 
+class Timer
+{
+public:
+	Timer()
+	{
+		m_StartTimepoint = chrono::high_resolution_clock::now();
+	}
+	
+	~Timer()
+	{
+		Stop();
+	}
+
+	void Stop()
+	{
+		auto endTimepoint = chrono:high_resolution_clock::now();
+
+		auto start = chromo:: time_point_cast < chrono::microseconds > (m_StartTimepoint).time_since_epoch().count();
+		auto end = chromo:: time_point_cast < chrono::microseconds > (endTimepoint).time_since_epoch().count();
+
+		auto duration = end - start;
+		double ms = duration * 0.001;
+
+		cout << duration << "us (" << ms << "ms)\n";
+	}
+private: 
+	chrono::time_point< chrono:high_resolution_clock> m_StartTimepoint;
+};
 
 int main()
 {
