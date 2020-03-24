9f46921fe8553b29c458014c72e5b3fbe55f5594
Tahj <tahjchisolm21@gmail.com>
2020-03-24 13:36:07 -0400

    take in the input

--- recursion.s
+++ recursion.s
@@ -12,6 +12,12 @@ main:
     li $v0, 4
     syscall
 
+    li $v0, 8 #takes input
+    la $a0, buffer #byte space
+    li $a1, 1000
+    move $t0, $a0 #string stored in $t0
+    syscall
+
     jr $ra
 
 subprog:
