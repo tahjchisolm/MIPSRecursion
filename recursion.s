342e07c8c73642e228117adb192c4115727241b9
Tahj <tahjchisolm21@gmail.com>
2020-03-31 20:16:52 -0400

    restart

--- recursion.s
+++ recursion.s
@@ -1,31 +1,30 @@
 .data
 myMessage: .asciiz "Enter your string:\n"
-invalidMessage: .asciiz "Invalid input"
+invalidMessage: .asciiz "NaN"
 input: .space 1001
 
 .text
+
 main:
 
-    jal subprog
+    li $t0, 2839480
+    li $t1, 11
+    div $t0, $t1
+    mfhi $t0 # store remainder
+    addi $s0, $t0, 26 # base N
+    addiu $s1, $s0, -10 # M
 
-    la $a0, myMessage
     li $v0, 4
-    syscall
+    la $a0, myMessage
+    syscall # print myMessage
 
-    li $v0, 8 #takes input
-    la $a0, buffer #byte space
-    li $a1, 1000
-    move $t0, $a0 #string stored in $t0
-    syscall
+    li $v0, 8
+    la $a0, input
+    li $a1, 1001
+    syscall # take input
 
-    li $t1, 2839480
-    li $t2, 11
-    div $t1, $t2
-    mfhi $t1 # store remainder
-    addi $s0, $t1, 26 # base N in $s0
-    addiu $s1, $s0, -10 # M in $s1
-
-    jr $ra
+    addi $s2, $zero, 0
+    add $s3, $s2, $zero
 
 subprog:
 
